7293a67e003b245f38d82de5880160c7bda1033d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Web.ASPxTreeList;
+using DevExpress.Web.ASPxTreeList.Internal;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+    protected void ASPxTreeListDemo_HtmlRowPrepared(object sender, DevExpress.Web.ASPxTreeList.TreeListHtmlRowEventArgs e)
+    {
+        ASPxTreeList treeList = (ASPxTreeList)sender;
+        WebControl selectionCheckbox = null;
+        foreach (TableCell cell in e.Row.Cells)
+        {
+            TreeListSelectionCell selectionCell = cell as TreeListSelectionCell;
+            if (selectionCell != null)
+            {
+                selectionCheckbox = (WebControl)selectionCell.Controls[0];
+                break;
+            }
+        }
+        if (selectionCheckbox != null)
+        {
+            TreeListNode node = treeList.FindNodeByKeyValue(e.NodeKey);
+            if (node.HasChildren)
+                selectionCheckbox.Enabled = false;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to disable selection checkboxes in the ASPxTreeList
+
+
+<p>This example demonstrates how to disable the selection checkboxes in the ASPxTreeList in the <a href="http://documentation.devexpress.com/#AspNet/DevExpressWebASPxTreeListASPxTreeList_HtmlRowPreparedtopic"><u>ASPxTreeList.HtmlRowPrepared</u></a> event handler.</p><p><strong>Note:</strong> This approach does not work with the <a href="http://community.devexpress.com/blogs/aspnet/archive/2011/04/26/asp-net-check-box-new-render-state-for-multiple-controls-coming-soon-in-2011-volume-1.aspx"><u>Three-state checkbox</u></a> feature implementation starting with version v2011 vol 1.</p>
+
+
+<h3>Description</h3>
+
+<p>The selection checkbox reference can be get in one of the Cells.Controls hierarchy.</p><p>This approach does not work with the <a href="http://community.devexpress.com/blogs/aspnet/archive/2011/04/26/asp-net-check-box-new-render-state-for-multiple-controls-coming-soon-in-2011-volume-1.aspx"><u>Three-state checkbox</u></a> feature implementation starting with version v2011 vol 1.</p>
+
+<br/>
+
+
